67ad37c04d1186965a671390392bb54737d86619
43EVER <i@43ever.me>
2019-10-28 00:49:15 +0800

    准备做界面了鸭

--- Node.js_Vue.js_MongoDB/admin/src/views/Main.vue
+++ Node.js_Vue.js_MongoDB/admin/src/views/Main.vue
@@ -1,16 +1,11 @@
 <template>
   <el-container style="height: 100vh;">
     <el-aside width="200px" style="background-color: rgb(238, 241, 246)">
-      <el-menu router :default-openeds="['1', '3']">
+      <el-menu router :default-openeds="['1']">
         <el-submenu index="1">
           <template slot="title">
             <i class="el-icon-message"></i>内容管理
           </template>
-          <el-menu-item-group>
-            <template slot="title">分类</template>
-            <el-menu-item index="/categories/create">新建分类</el-menu-item>
-            <el-menu-item index="/categories/list">分类列表</el-menu-item>
-          </el-menu-item-group>
           <el-menu-item-group>
             <template slot="title">物品</template>
             <el-menu-item index="/items/create">新建物品</el-menu-item>
@@ -25,6 +20,31 @@
             <template slot="title">文章</template>
             <el-menu-item index="/articles/create">新建文章</el-menu-item>
             <el-menu-item index="/articles/list">文章列表</el-menu-item>
+          </el-menu-item-group>
+        </el-submenu>
+        <el-submenu index="2">
+          <template slot="title">
+            <i class="el-icon-message"></i>运营管理
+          </template>
+          <el-menu-item-group>
+            <template slot="title">广告</template>
+            <el-menu-item index="/ads/create">新建广告位</el-menu-item>
+            <el-menu-item index="/ads/list">广告位列表</el-menu-item>
+          </el-menu-item-group>
+        </el-submenu>
+        <el-submenu index="3">
+          <template slot="title">
+            <i class="el-icon-message"></i>系统设置
+          </template>
+          <el-menu-item-group>
+            <template slot="title">分类</template>
+            <el-menu-item index="/categories/create">新建分类</el-menu-item>
+            <el-menu-item index="/categories/list">分类列表</el-menu-item>
+          </el-menu-item-group>
+          <el-menu-item-group>
+            <template slot="title">用户</template>
+            <el-menu-item index="/ads/create">新建用户</el-menu-item>
+            <el-menu-item index="/ads/list">用户列表</el-menu-item>
           </el-menu-item-group>
         </el-submenu>
       </el-menu>
